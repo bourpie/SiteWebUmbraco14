ba0a2fde751813e9f5e6f3887e90d1727d2909ee
Pierre Bourgeois <pbourgeois@soquij.qc.ca>
2024-08-08 07:08:10 -0400

    first commit

---
+++ Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace SiteWeb.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static Accueil? GetAccueil(this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<Accueil>();
+        }
+
+        public static ParametresGlobaux? GetParametresGlobaux(this IPublishedContent publishedContent)
+        {
+            var accueil = GetAccueil(publishedContent);
+            if (accueil == null) return null;
+            return accueil.FirstChild<ParametresGlobaux>();
+        }
+    }
+}
